ac1e494d53d52fd099be3015f37c12d6b018e56b
niels-b01 <115983200+niels-b01@users.noreply.github.com>
2022-10-26 19:15:00 +0200

    dummy vars test

--- prediction-ui-embedded-gcs/heartattack_predictor.py
+++ prediction-ui-embedded-gcs/heartattack_predictor.py
@@ -13,6 +13,7 @@ class HeartAttackPredictor:
         print(prediction_input)
         cat_cols = ['sex','exng','caa','cp','fbs','restecg','slp','thall']
         con_cols = ["age","trtbps","chol","thalachh","oldpeak"]
+        catvals = [0,1,2,3]
 
         model_name = os.environ.get('MODEL_NAME', 'Specified environment variable is not set.')
         if self.model is None:
@@ -20,13 +21,14 @@ class HeartAttackPredictor:
 
         df = pd.read_json((prediction_input).to_json(), orient='records')
 
-        df = pd.get_dummies(df, columns = cat_cols, drop_first = True)
+        df_dummified = pd.get_dummies(df[cat_cols].astype(pd.CategoricalDtype(categories=catvals)))
+        df_dummified.insert(0, con_cols, df[con_cols])
 
         # instantiate the scaler
         scaler = RobustScaler()
 
         # scaling the continuous features
-        df[con_cols] = scaler.fit_transform(df[con_cols])
+        df_dummified[con_cols] = scaler.fit_transform(df[con_cols])
 
-        y_pred = self.model.predict(df)
+        y_pred = self.model.predict(df_dummified)
         return y_pred
