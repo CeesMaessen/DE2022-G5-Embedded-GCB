6e5db4cbc92ba549018909d1fa1124494a630239
Maessen <c.j.w.maessen@student.tue.nl>
2022-10-26 16:50:32 +0200

    added json foreal

--- prediction-ui-embedded-gcs/heartattack_predictor.py
+++ prediction-ui-embedded-gcs/heartattack_predictor.py
@@ -13,6 +13,6 @@ class HeartAttackPredictor:
         model_name = os.environ.get('MODEL_NAME', 'Specified environment variable is not set.')
         if self.model is None:
             self.model = joblib.load(model_name)
-        df = pd.read_json(to_json(prediction_input), orient='records')
+        df = pd.read_json((prediction_input).to_json(), orient='records')
         y_pred = self.model.predict(df)
         return y_pred
